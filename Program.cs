f019b0adedac4e48cf1f4b79b639a714679d466b
GabrielPS <gabrielpereirx33@gmail.com>
2025-04-10 22:20:23 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,128 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using principal;
 
-namespace contaCorrente
+namespace Comissao
 {
-    public class ContaCorrente
+    public class comissao
     {
-        private string nome;
-        private string cpf;
-        private double limiteCredito;
-        private double saldo;
-        private bool negativado;
-
-        public string getNome()
-        {
-            return nome;    
-        }
-
-        public void setnome(string nome)
-        {
-            this.nome = nome;
-        }
-
-        public string getcpf()
-        {
-            return cpf;
-        }
-            public void setcpf(string cpf)
-        {
-            this.cpf = cpf;
-        }
-
-        public double getlimiteCredito()
-        {
-            return limiteCredito;   
-        }
-
-        public void setlimiteCredito(double limiteCredito)
-        {
-            this.limiteCredito = limiteCredito;
-
-        }
-
-        public double getsaldo()
-        {
-            return saldo;
-        }
-
-        public void setsaldo(double saldo)
-        {
-            this.saldo = saldo; 
-
-        }
-
-        public bool getnegativado()
-        {
-            return negativado;  
-        }
-
-        public void setnegativado(bool negativado)
-        {
-            this.negativado = negativado;
-        }
-
-        public double depositar(double valor)
-        {
-            saldo += valor;
-            return saldo;
-        }
-
-
-
-        public double sacar(double saque, double limite)
-
-        {
-         
-            if (saque <= saldo)
-            {
-                saldo -= saque;
-                Console.WriteLine("Saque no valor de " + saque + " foi realizado!");
-                return saldo;
-            }
-
-
-
-            else
-
-            {
-                Console.WriteLine("O seu saldo é insuficientte para esse saque!");
-                negativado = true;
-
-                limite -= saque;
-                limite += saldo;
-
-                limiteCredito = limite;
-
-                return limiteCredito;
-
-            }
-
-
-        }
-        
-
-           
-
-        
-            
-
-
-
-
-        public void verSaldo()
-        {
-            Console.WriteLine("O saldo atual é " + saldo);
-
-
-        }
-
        
+        private string nome;
+        private double salarioBase;
+       
+        
+
+        public string getnome() { return nome; }
+        
+        public double getvalorVendido() { return salarioBase; }
+      
+        public void setnome(string nome) { this.nome = nome; }
+        public void setsalarioBase(double salarioBase) { this.salarioBase = salarioBase; }
+      
+
+       public double contacomissao(double valorVendido)
+        {
+            valorVendido = valorVendido * (0.03 + (salarioBase * 0.00001));
+            Console.WriteLine("\n A comissão é estimada no valor de R$" + valorVendido);
+
+            return valorVendido;
+        }
+        
+
+
+        
     }
 }
---
+++ principalcomissao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Comissao;
+
+
+namespace principal
+{
+    public class principalcomissao
+
+    {
+        public static void Main(string[] args)
+        {
+            comissao c1
+                = new comissao();
+            Console.WriteLine("Qual seu nome?\n");
+            c1.setnome(Console.ReadLine());
+            Console.WriteLine("Qual seu salário atual?\n");
+            c1.setsalarioBase(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Qual foi o valor da venda?\n");
+                double valorVendido = Convert.ToDouble(Console.ReadLine());
+            c1.contacomissao(valorVendido);
+          
+        }
+
+    }
+}
